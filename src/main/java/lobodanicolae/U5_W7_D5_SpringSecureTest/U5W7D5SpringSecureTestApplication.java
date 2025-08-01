6474ffb235fb8f030c5b9190f63d36aae795c724
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-08-01 11:37:08 +0200

    initialize Spring Boot project

---
+++ src/main/java/lobodanicolae/U5_W7_D5_SpringSecureTest/U5W7D5SpringSecureTestApplication.java
@@ -0,0 +1,13 @@
+package lobodanicolae.U5_W7_D5_SpringSecureTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W7D5SpringSecureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W7D5SpringSecureTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lobodanicolae/U5_W7_D5_SpringSecureTest/U5W7D5SpringSecureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lobodanicolae.U5_W7_D5_SpringSecureTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W7D5SpringSecureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
