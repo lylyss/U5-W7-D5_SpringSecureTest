0c0c0627aebf74cdf3c3f7d9182e774e4a8c37dc
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-08-01 16:49:21 +0200

    Add event retrieval endpoint and refactor Event entity date type

--- src/main/java/lobodanicolae/U5_W7_D5_SpringSecureTest/security/SecurityConfig.java
+++ src/main/java/lobodanicolae/U5_W7_D5_SpringSecureTest/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/events").permitAll()
                         .anyRequest().authenticated()
                 );
 
